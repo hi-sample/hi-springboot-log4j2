c839e623970bdf866cdc7a59d781d7e710d87c25
ikin <ikin@hifor.net>
2024-04-01 14:58:56 +0800

    update

--- src/main/java/net/hifor/demo/controller/HelloSpringbootController.java
+++ src/main/java/net/hifor/demo/controller/HelloSpringbootController.java
@@ -5,6 +5,8 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 /**
  * @author IKin <br/>
  * @description 第一个springboot  <br/>
@@ -24,6 +26,14 @@ public class HelloSpringbootController {
     public String hello() {
         String hello = "Hello Spring boot!";
         logger.info(hello);
+        int i = 0;
+        while (true) {
+            logger.info(System.currentTimeMillis() + " :: " + i + " :: " + UUID.randomUUID());
+            i++;
+            if (i >= 10000) {
+                break;
+            }
+        }
         return hello;
     }
 }
